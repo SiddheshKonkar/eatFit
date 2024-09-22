a108ab0a4f1c1506ada527079e0689235ea88415
SiddheshKonkar <apcoson1@gmail.com>
2024-09-22 21:36:15 +0530

    Tailwind CSS Configured

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="text-purple-800 font-semibold text-4xl text-center">This is a Header</div>
+  )
+}
+
+export default Header
