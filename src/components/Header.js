ef1e47f4d7e719787272f4eb5e0837558d325120
Your Name <apcoson1@gmail.com>
2024-09-24 23:04:41 +0530

    Header Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+
 
 const Header = () => {
   return (
-    <div className="text-purple-800 font-semibold text-4xl text-center">This is a Header</div>
-  )
-}
+    <>
+      <div className="flex items-center px-5 justify-around bg-amber-100 ">
+        <a href="/">
+          <img src="../assets/biteboxLogo.png" className="h-24 w-24" />
+        </a>
+        <div className="flex gap-14">
+          <a className="text-amber-700 font-semibold	text-lg" href="#">
+            Home
+          </a>
+          <a className="text-amber-700 font-semibold	text-lg" href="#">
+            About
+          </a>
+          <a className="text-amber-700 font-semibold	text-lg" href="#">
+            Menu
+          </a>
+          <a className="text-amber-700 font-semibold	text-lg" href="#">
+            Contact
+          </a>
+        </div>
+        <a className="text-amber-700 font-semibold	text-lg" href="#">
+          Log in
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
